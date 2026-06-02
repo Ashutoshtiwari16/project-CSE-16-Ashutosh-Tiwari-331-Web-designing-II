2d4c2703bd45d57f2aa05171648940721b0b2575
Ashutoshtiwari16 <ashutoshtiwariayc@gmail.com>
2026-06-02 13:09:04 +0530

    Setup GitHub Pages

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/project-CSE-16-Ashutosh-Tiwari-331-Web-designing-II/'
 })
